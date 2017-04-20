d1bb4da0c37e065ce9c418ce85589426c9f936b8
fsz <fsz65@126.com>
2017-04-20 14:35:32 +0800

    增加项目内容

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # hapi-demo
 learn hapi 
+
+**demo1:**<br/>
+1、主要展示了hapi的路由概念及url参数问题<br>
+2、<br>
+```
+cd hapi-demo
+npm ./demo1/server.js
+```
---
+++ demo1/server.js
@@ -0,0 +1,29 @@
+/**
+ * Created by 15061857 on 2017/4/20.
+ */
+'use strict'
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+server.connection({port:3000,host:'localhost'});
+
+server.route({
+    method:'GET',
+    path:'/',
+    handler:function (request,reply) {
+        reply('Hello, world');
+    }
+});
+
+server.route({
+    method:'GET',
+    path:'/{name}',
+    handler:function (request,reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+server.start((err) => {
+    if (err){
+        throw err;
+    }
+    console.log('Server running at:$(server.info.uri)');
+});
