2269c4b38af98c751c9ff6d55cde2357dbfc2dca
fsz <fsz65@126.com>
2017-04-20 16:14:42 +0800

    增加项目内容，增加静态页的加载

--- demo1/server.js
+++ demo1/server.js
@@ -6,6 +6,7 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({port:3000,host:'localhost'});
 
+// 关于hapi的路由
 server.route({
     method:'GET',
     path:'/',
@@ -21,6 +22,26 @@ server.route({
         reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
     }
 });
+
+// 支持静态页面  开始代码
+server.register(require('inert'),(err) => {
+    if (err){
+        throw err;
+    }
+    server.route({
+        method:'GET',
+        path:'/hello',
+        handler:function (request,reply) {
+            /*一般情况下要带参数confine:false,否则会出现403错误
+            * 还有一种情况要注意，如果server的启动命令是 node ./demo1/server.js 那么相对路径要写成demo1/public的形式，否则报404错误
+            * http://npm.taobao.org/package/inert
+            * https://hapijs.com/tutorials/serving-files
+            * */
+            reply.file('demo1/public/hello.html',{confine:false});
+        }
+    });
+});
+// 支持静态页面  结束代码
 server.start((err) => {
     if (err){
         throw err;
