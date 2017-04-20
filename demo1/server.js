ceb8d610b82833685791d0d28a9e99b9daefa8c4
fsz65 <fsz65@126.com>
2017-04-20 21:10:07 +0800

    修改代码

--- demo1/server.js
+++ demo1/server.js
@@ -46,5 +46,5 @@ server.start((err) => {
     if (err){
         throw err;
     }
-    console.log('Server running at:$(server.info.uri)');
+    console.log(`Server running at:$(server.info.uri)`);
 });
